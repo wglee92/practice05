77c094190d28c6a952a50d0c683126b49210ae01
bit-user <bit-user@bit>
2017-04-20 12:56:07 +0900

    stack generic 적용

--- src/prob5/MainApp.java
+++ src/prob5/MainApp.java
@@ -4,7 +4,7 @@ public class MainApp {
 
 	public static void main(String[] args) {
 		try {
-			MyStack stack = new MyStack(3);
+			MyStack<String> stack = new MyStack<String>(3);
 			stack.push("Hello");
 			stack.push("World");
 			stack.push("!!!");
@@ -12,7 +12,12 @@ public class MainApp {
 			stack.push(".");
 
 			while (stack.isEmpty() == false) {
-				System.out.println(stack.pop());
+				// 런타임 에러
+				// generic을 사용하지 않으면 코드 작성시 타입 체크를 하지 않음
+				// Integer s = (Integer)stack.pop();
+				String s = stack.pop();
+				
+				System.out.println( s );
 			}
 
 			System.out.println("======================================");
